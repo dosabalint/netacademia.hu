b9819b6e58160ed780490279e6dadd5c6b72d88e
Nagy Gergo <nagy.gergo@3pixel.hu>
2018-06-06 11:42:39 +0200

    DEV-31 tanfolyamkereso loading.gif fix

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -3,7 +3,7 @@ import VueLazyload from 'vue-lazyload'
 
 Vue.use(VueLazyload, {
   preLoad: 1.3,
-  error: 'loading.gif',
-  loading: 'loading.gif',
+  error: require('~/assets/loading.gif'),
+  loading: require('~/assets/loading.gif'),
   attempt: 1
 });

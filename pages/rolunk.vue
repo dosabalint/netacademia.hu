376d05882e22389c0c5143446ce71094dea8097b
dosabalint <dosa.balint@gmail.com>
2018-08-01 12:19:11 +0200

    DEV-81 rólunk oldal átemelés

---
+++ pages/rolunk.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <iframe src="/page-rolunk/index.html" frameborder="0"
+            :style="{height: iframe.height + 'px'}"
+            @load="IframeResize"></iframe>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "vue-path",
+    layout: "decode",
+    data: function() {
+      return {
+        iframe: {
+          height: 0
+        }
+      };
+    },
+    methods: {
+      IframeResize(event) {
+        this.iframe.height = event.target.contentWindow.document.body.scrollHeight;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  iframe {
+    width: 100%;
+  }
+</style>

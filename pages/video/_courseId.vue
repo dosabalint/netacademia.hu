e7805b80b24589932a73d8c289b81be15846d55e
dosabalint <dosa.balint@gmail.com>
2018-08-24 23:37:28 +0200

    DEV-334 kurzusnév

---
+++ pages/video/_courseId.vue
@@ -0,0 +1,197 @@
+<template>
+  <main class="pb-5">
+    <!-- cover -->
+    <div class="bg-indulo">
+      <div class="container h-100">
+        <div class="h-100 d-flex align-items-center">
+          <h1 class="m-0 p-0">{{ courseName }}</h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mt--80px">
+      <div class="row video-box">
+        <div class="col-lg-7 col-xl-8 my-auto px-0">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" :src="vimeoUrl" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="col-lg-5 col-xl-4 playlist px-0 border-bottom">
+          <div class="almost-standart">
+
+            <div class="big-block border-bottom">
+
+              <div v-for="(playlistItem, index) in playlist" :key="index"
+                   class="row py-3"
+                   :class="{
+                      'border-bottom': index !== playlist.length - 1
+                    }">
+                <div class="col-2 my-auto">
+                  <a href="#" @click.prevent="openVideo('f9f46536-4ff0-4a96-af08-74a1755bfc91')">
+                    <img src="~/assets/video/play-button.svg" width="20" alt="" class="ml-5 ml-lg-3 w-20px">
+                  </a>
+                </div>
+                <div class="col-7 my-auto">
+                  <a href="#">
+                    {{ index }} Linux alapok, Raspbian rendszer sajátosságai 1
+                  </a>
+                </div>
+                <div class="col-2 mx-auto my-auto">
+                  22:25
+                </div>
+                <div class="col-lg-8 text-center mt-2">
+                  <a href="#">
+                    <img src="~/assets/video/musical-note.svg" height="15" alt="" class="mr-2 w-15px">
+                    <small>mp3 letöltése</small>
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-12 p-4 mt-3">
+          <h5>Aktuális videó címe</h5>
+          <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vero reiciendis dolorum sunt
+            dolor commodi tenetur, harum consequatur quidem, totam eius maxime, sint dolore quod dolorem cum, doloribus
+            recusandae repudiandae?</p>
+        </div>
+      </div>
+      <!--videobox row-end-->
+    </div>
+  </main>
+</template>
+
+<script>
+
+  export default {
+    name: "videoPage",
+    layout: "decode",
+    data: function() {
+      return {
+        vimeoId: null,
+        pageData: null,
+        playlist: new Array(7),
+        vimeoUrl: null,
+        courseName: null
+      };
+    },
+    computed: {
+      /**
+       * @return {string}
+       */
+      ApiVideoUrl() {
+        return `${this.$store.state.url.backend}${this.$store.state.url.videos}/${this.CourseId}`;
+      },
+      /**
+       * @return {string}
+       */
+      ApiVideoPlayerBaseUrl() {
+        return `${this.$store.state.url.backend}${this.$store.state.url.videosPopup}`;
+      },
+      CourseId() {
+        return this.$route.params.courseId;
+      },
+      VimeoId: {
+        get: function() {
+          return this.vimeoId;
+        },
+        set: function(newVimeoId) {
+          this.vimeoId = newVimeoId;
+          this.updateVimeoUrl(this.newValue);
+        }
+      }
+    },
+    created() {
+      this.fetchPageData()
+        .then(response => response.data)
+        .then(pageData => {
+          this.pageData = pageData;
+          this.loadPlaylist(pageData.Modules);
+
+          console.log(pageData);
+          this.courseName = pageData.Title;
+        });
+    },
+    methods: {
+      fetchPageData() {
+        return this.$axios.get(`${this.ApiVideoUrl}`, { withCredentials: true });
+      },
+      loadPlaylist(modules) {
+        // let playlist = pageData.Modules.map(module => {
+        //   return module.Recordings.map(recording => {
+        //     return {};
+        //   });
+        // });
+        console.log(modules);
+      },
+      openVideo(videoID) {
+        this.$axios.get(`${this.ApiVideoPlayerBaseUrl}/${videoID}`, { withCredentials: true })
+          .then(response => response.data)
+          .then(videoData => {
+            this.VimeoId = videoData.Id;
+          });
+      },
+      updateVimeoUrl() {
+        console.log(`${this.$store.state.url.vimeoPlayer}/${this.vimeoId}`);
+        this.vimeoUrl = `${this.$store.state.url.vimeoPlayer}/${this.vimeoId}`;
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+
+  main {
+    background: #f1f6f9;
+  }
+
+  .bg-indulo {
+    overflow: auto;
+    height: 300px;
+    background: #fff url("~/assets/video/indulo-bg.svg") no-repeat;
+    background-size: cover;
+  }
+
+  h1 {
+    padding-top: 100px;
+    color: #fff !important;
+    font-size: 2.5rem !important;
+  }
+
+  .video-box {
+    background: rgba(250, 250, 250, 1);
+
+    a {
+      color: #434343;
+
+      &:hover {
+        text-decoration: none;
+        color: #2d9ad2;
+      }
+    }
+  }
+
+  .playlist {
+    height: 428px;
+    overflow-y: auto;
+    overflow-x: hidden;
+  }
+
+  /* margin */
+
+  .mt--80px {
+    margin-top: -80px;
+  }
+
+  /* width */
+
+  .w-15px {
+    width: 15px;
+  }
+
+  .w-20px {
+    width: 20px;
+  }
+</style>

674e1d414fd19e541317225d71b8ec69ade927c5
dosabalint <dosa.balint@gmail.com>
2018-09-10 00:32:33 +0200

    DEV-457 nuxt env bővítés firebase configgal

--- nuxt.config.js
+++ nuxt.config.js
@@ -131,7 +131,15 @@ module.exports = {
   },
   env: {
     baseUrl: isProd() ? process.env.BASE_URL : process.env.TEST_BASE_URL,
-    backendUrl: isProd() ? process.env.BACKEND_URL : process.env.TEST_BACKEND_URL
+    backendUrl: isProd() ? process.env.BACKEND_URL : process.env.TEST_BACKEND_URL,
+    firebase: {
+      apiKey: process.env.FB_API_KEY,
+      authDomain: process.env.FB_AUTH_DOMAIN,
+      databaseUrl: process.env.FB_DATABASE_URL,
+      projectId: process.env.FB_PROJECT_ID,
+      storageBucket: process.env.FB_STORAGE_BUCKET,
+      messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
+    }
   },
   axios: {
     https: process.env.NODE_ENV === "production" ? true : false,
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,12 +3,12 @@ import "@firebase/database";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    databaseURL: process.env.FB_DATABASE_URL,
-    projectId: process.env.FB_PROJECT_ID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
+    apiKey: process.env.firebase.apiKey,
+    authDomain: process.env.firebase.authDomain,
+    databaseURL: process.env.firebase.databaseUrl,
+    projectId: process.env.firebase.projectId,
+    storageBucket: process.env.firebase.storageBucket,
+    messagingSenderId: process.env.firebase.messagingSenderId
   });
 }
 
